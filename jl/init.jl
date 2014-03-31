ace1312ae6225ebce653adc567ffbb95d1e9b746
Mike Innes <mike.j.innes@gmail.com>
2014-03-31 14:14:05 +0100

    fix typo

--- jl/init.jl
+++ jl/init.jl
@@ -5,7 +5,7 @@ try
 catch e
   if isa(e, ErrorException) && e.msg == "Jewel not found"
     try
-      info("Couldn't find Jewel package, attempting manual instal...")
+      info("Couldn't find Jewel package, attempting manual install...")
       Pkg.add("Jewel")
     catch e
       println(STDERR, """
