1b348ae1776651192d0d6672f821f56a8ef36162
Mike Innes <mike.j.innes@gmail.com>
2014-04-02 11:39:41 +0100

    Update init.jl

--- jl/init.jl
+++ jl/init.jl
@@ -5,13 +5,13 @@ try
 catch e
   if isa(e, ErrorException) && e.msg == "Jewel not found"
     try
-      info("Couldn't find Jewel package, attempting manual install...")
+      info("Couldn't find Jewel package, attempting installation...")
       Pkg.add("Jewel")
     catch e
       println(STDERR, """
       -----------------------------------------------
       We couldn't install Jewel.jl for you.
-      Try executing Pkg.add("Jewel") in a Julia repl.
+      Try using Pkg.add("Jewel") in a Julia repl.
       -----------------------------------------------
       """)
       rethrow()
