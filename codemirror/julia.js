2c00d28b18cc69ce3197fd3b11e59d210291a633
Michael Hatherly <michaelhatherly@gmail.com>
2014-06-03 19:51:36 +0200

    Add <: as binary operator.

--- codemirror/julia.js
+++ codemirror/julia.js
@@ -34,7 +34,7 @@ CodeMirror.defineMode("julia2", function(config, parserConfig) {
     return new RegExp("^(" + words.join("|") + ")\\b");
   }
 
-  var binary_operatos = /^(?:&&|\|\||::|==|!=|=|[<>]=?|\?|\:|[*\/\+-]=?|[≤≥=&\|])/
+  var binary_operatos = /^(?:&&|\|\||::|<:|==|!=|=|[<>]=?|\?|\:|[*\/\+-]=?|[≤≥=&\|])/
   var operators   = /^(?:\.?[|&^\\%*+\-<>!=\/]=?|\?|~|::|:|\$|<:|\.[<>]|<<=?|>>>?=?|\.[<>=]=|->?|\/\/|\bin\b|\.{3}|\.)/;
   var delimiters  = /^[;,()[\]{}]/;
   var identifiers = /^[_A-Za-z][_A-Za-z0-9!]*/;
