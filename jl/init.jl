39b7f983b730843f8480b626c596d2259b3f2d62
Michael Hatherly <michaelhatherly@gmail.com>
2014-07-23 16:19:02 +0200

    Use `exit` rather than else clause.

--- jl/init.jl
+++ jl/init.jl
@@ -3,34 +3,33 @@ port, id = map(parseint, ARGS)
 if VERSION < v"0.3-"
   println(STDERR, """
   This plugin requires Julia 0.3 or higher to work. Your current version
-  is $(VERSION). Please download a more recent version of Julia from
+  is $(VERSION). Please download a more recent release from
   http://www.julialang.org/downloads
   """)
-else
+  exit(1)
+end
 
-  try
-    require("Jewel")
-  catch e
-    if isa(e, ErrorException) && e.msg == "Jewel not found"
-      try
-        info("Couldn't find Jewel package, attempting installation...")
-        Pkg.add("Jewel")
-      catch e
-        println(STDERR, """
-        -----------------------------------------------
-        We couldn't install Jewel.jl for you.
-        Try using Pkg.add("Jewel") in a Julia repl.
-        -----------------------------------------------
-        """)
-        rethrow()
-      end
-    else
+try
+  require("Jewel")
+catch e
+  if isa(e, ErrorException) && e.msg == "Jewel not found"
+    try
+      info("Couldn't find Jewel package, attempting installation...")
+      Pkg.add("Jewel")
+    catch e
+      println(STDERR, """
+      -----------------------------------------------
+      We couldn't install Jewel.jl for you.
+      Try using Pkg.add("Jewel") in a Julia repl.
+      -----------------------------------------------
+      """)
       rethrow()
     end
+  else
+    rethrow()
   end
-
-  require("Jewel")
-
-  Jewel.server(port, id)
-
 end
+
+require("Jewel")
+
+Jewel.server(port, id)
