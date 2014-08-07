86ed2b0d52cb99b5e60c943f25ec7d87bf886b7a
Mike Innes <mike.j.innes@gmail.com>
2014-08-07 15:53:20 +0100

    clean up namespace

--- src/julia/profiler.cljs
+++ src/julia/profiler.cljs
@@ -1,32 +1,12 @@
 (ns lt.objs.langs.julia.profiler
   (:require [lt.objs.langs.julia.util :as util]
             [lt.object :as object]
-            [lt.objs.eval :as eval]
             [lt.util.dom :as dom]
-            [lt.objs.console :as console]
-            [lt.objs.command :as cmd]
-            [lt.objs.clients.tcp :as tcp]
-            [lt.objs.sidebar.clients :as scl]
-            [lt.objs.dialogs :as dialogs]
-            [lt.objs.files :as files]
-            [lt.objs.popup :as popup]
-            [lt.objs.platform :as platform]
-            [lt.plugins.auto-complete :as auto-complete]
-            [lt.objs.editor :as ed]
-            [lt.objs.plugins :as plugins]
-            [lt.plugins.watches :as watches]
-            [clojure.string :as string]
-            [lt.objs.clients :as clients]
-            [lt.objs.notifos :as notifos]
-            [lt.objs.cache :as cache]
-            [lt.util.load :as load]
-            [lt.util.cljs]; :refer [js->clj]]
             [lt.objs.editor :as editor]
             [lt.objs.editor.pool :as pool]
-            [lt.plugins.doc :as doc]
             [crate.core :as crate]
             [lt.objs.menu :as menu])
-  (:require-macros [lt.macros :refer [behavior defui]]))
+  (:require-macros [lt.macros :refer [behavior]]))
 
 ;; CodeMirror extension
 
