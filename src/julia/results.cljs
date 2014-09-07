f0eb6d69f470d0aa99a8f396aa0cc076bf61bf52
Mike Innes <mike.j.innes@gmail.com>
2014-09-06 22:28:31 -0400

    tweaks to interop, disable collapsible processing

--- src/julia/results.cljs
+++ src/julia/results.cljs
@@ -6,37 +6,39 @@
             [lt.objs.clients :as clients])
   (:require-macros [lt.macros :refer [behavior defui]]))
 
-(defn ->ed [ed]
-  (cond (integer? ed) (@object/instances ed)
-        :else ed))
+(def results (atom {}))
 
-(defn jleval [ed code]
+(defn ->ed [thing]
+  (cond
+   (string? thing) (some-> @results (get thing) deref :ed)
+   (integer? thing) (@object/instances ed)
+   :else thing))
+
+(defn jlcall [ed code]
   (let [ed (->ed ed)
         client (-> @ed :client :default)]
     (when client
       (clients/send client :eval.julia code
                     :only ed))))
 
-(set! js/jleval jleval)
+(set! js/jlcall jlcall)
 
-(defn process-collapsible! [ed dom]
-  (let [header (dom/$ :.collapsible-header dom)
-        content (js/$ (dom/$ :.collapsible-content dom))]
-    (.hide content)
-    (set! (.-onclick header)
-          (fn []
-            (.toggle content 200)
-            (js/setTimeout #(editor/refresh ed) 200)))))
+;; (defn process-collapsible! [ed dom]
+;;   (let [header (dom/$ :.collapsible-header dom)
+;;         content (js/$ (dom/$ :.collapsible-content dom))]
+;;     (.hide content)
+;;     (set! (.-onclick header)
+;;           (fn []
+;;             (.toggle content 200)
+;;             (js/setTimeout #(editor/refresh ed) 200)))))
 
-(defn process-collapsibles! [res]
-  (doseq [collapsible (dom/$$ :.collapsible (@res :result))]
-    (process-collapsible! (:ed @res) collapsible)))
+;; (defn process-collapsibles! [res]
+;;   (doseq [collapsible (dom/$$ :.collapsible (@res :result))]
+;;     (process-collapsible! (:ed @res) collapsible)))
 
-(defn show-collapsibles! [dom]
-  (when-not (string? dom)
-    (->> dom (dom/$$ :.collapsible-content) js/$ .show)))
-
-(def results (atom {}))
+;; (defn show-collapsibles! [dom]
+;;   (when-not (string? dom)
+;;     (->> dom (dom/$$ :.collapsible-content) js/$ .show)))
 
 (behavior ::inline-results
           :triggers #{:editor.result}
@@ -53,8 +55,8 @@
                                                     :line line
                                                     :id (:id opts)})]
                         (when (:id opts) (swap! results assoc (:id opts) res-obj))
-                        (when-not (string? res)
-                          (process-collapsibles! res-obj))
+;;                         (when-not (string? res)
+;;                           (process-collapsibles! res-obj))
                         (when-let [prev (get (@this :widgets) [line type])]
                           (when (:open @prev)
                             (object/merge! res-obj {:open true}))
